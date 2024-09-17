a980cf0c8bdaf70bdb7c1305d1d2016863cd3d62
OPRYAN90 <rfrjoon@gmail.com>
2024-09-17 16:46:28 -0700

    Another Path Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login'; // Import the Login component
 import GetStarted from './pages/GetStarted'; // Import the GetStarted component
-import Workpage from './pages/WorkPage';
+// import Workpage from './pages/WorkPage';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/get-started" element={<GetStarted />} /> 
-        <Route path="/workpage" element={<Workpage />} />
+        {/* <Route path="/workpage" element={<Workpage />} /> */}
       </Routes>
     </Router>
   );
